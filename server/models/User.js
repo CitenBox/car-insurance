18b448b84e9761059c80e36e9dc652496418aaa7
sapSap7 <sapir280@gmail.com>
2025-11-20 11:59:51 +0200

    p

--- server/models/User.js
+++ server/models/User.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema(
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    userPounts: { type: Number, default: 0 },
+    userPoints: { type: Number, default: 0 },
 
     test: [
       {
